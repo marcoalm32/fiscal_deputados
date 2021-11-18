c524df69cacf61d75bca5789925b503971baf6fa
marcoalm32 <68071619+marcoalm32@users.noreply.github.com>
2021-11-18 20:49:35 -0300

    criação do serviço pegarDeputados

--- fiscal-deputados/src/app/shared/service/deputado.service.ts
+++ fiscal-deputados/src/app/shared/service/deputado.service.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { DeputadosModule } from 'src/app/deputados/deputados.module';
+import { RespostaModel } from 'src/app/deputados/model/resposta.model';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -11,5 +14,8 @@ export class DeputadoService {
 
   constructor(private http: HttpClient) { }
 
+  pegarDeputados() : Observable<RespostaModel<DeputadosModule>> {
+    return this.http.get<RespostaModel<DeputadosModule>>(this.urlApi);
+  }
   
 }
