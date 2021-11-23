f03c00ebda43d416c2e6b340720ca0f1c182bdd2
marcoalm32 <68071619+marcoalm32@users.noreply.github.com>
2021-11-22 23:28:53 -0300

    correção do path '**' na rota deputados

--- fiscal-deputados/src/app/deputados/deputados-routing.module.ts
+++ fiscal-deputados/src/app/deputados/deputados-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeputadosComponent } from './components/deputados/deputados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: DeputadosComponent
+  },
+  {
+    path: 'despesas/:id',
+    loadChildren: () => import('../despesas/despesas.module').then(m => m.DespesasModule) 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
